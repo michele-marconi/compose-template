c0c9d15d3c521397aaefe05bb23006b0cf035e13
Michele Marconi <michel.marconi91@gmail.com>
2023-11-02 15:57:12 +0100

    Move searchbar outside Components class

--- app/src/main/java/com/spindox/composetemplate/ui/components/Components.kt
+++ app/src/main/java/com/spindox/composetemplate/ui/components/Components.kt
@@ -2,28 +2,18 @@ package com.spindox.composetemplate.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,17 +21,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -182,58 +168,6 @@ fun AlertDialogWithImage(
                 }
             }
         )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SearchBar(hintText: String, textToSearch: MutableState<String>, filteredList: List<Beer>) {
-    var active by rememberSaveable { mutableStateOf(false) }
-
-    SearchBar(
-        modifier = Modifier.padding(8.dp),
-        query = textToSearch.value,
-        onQueryChange = { textToSearch.value = it },
-        onSearch = { active = false },
-        active = active,
-        onActiveChange = { active = it },
-        placeholder = { Text(hintText) },
-        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
-    ) {
-        // Show the first 5 items from the filtered list
-        if (textToSearch.value.isNotEmpty()) {
-            filteredList.take(5).forEach { beer ->
-                ListItem(
-                    headlineContent = { Text(beer.name ?: "") },
-                    supportingContent = {
-                        Text(
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            text = beer.description ?: ""
-                        )
-                    },
-                    leadingContent = { Icon(Icons.Filled.FavoriteBorder, contentDescription = null) },
-                    modifier = Modifier
-                        .clickable {
-                            textToSearch.value = beer.name ?: ""
-                            active = false
-                        }
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 4.dp)
-                )
-            }
-        }
-        else{
-            Box(modifier = Modifier.fillMaxSize()) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .padding(16.dp),
-                    text = stringResource(R.string.no_elements_found),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-        }
     }
 }
 
